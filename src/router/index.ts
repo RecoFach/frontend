3fbc339ae6eeb3885bb09030371b5432b699bf8e
sierikov <artem.sierikov@gmail.com>
2020-12-02 01:49:13 +0100

    Add Login component - base

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Home
   },
   {
     path: '/about',
@@ -13,13 +13,18 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
+  }
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes
 });
 
 export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Login',
+  components: {
+
+  },
+});
+</script>
