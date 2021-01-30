1056b39797b858ed32c8c27edd753aac20ac4800
Alexander Chernykh <alexanderchernykh.ru@gmail.com>
2021-01-30 21:10:24 +0100

    Added multiple variables for CS areas

--- src/views/Interests.vue
+++ src/views/Interests.vue
@@ -3,9 +3,23 @@
     <h1>Users interests</h1>
     <div style="margin-top: 20px">
       <div class="Interest_checkbox" style="margin-top: 20px">
-        <el-checkbox v-model="checked3" label="af" border size="medium">{{
-          user.profile.username
-        }}</el-checkbox>
+        <el-checkbox v-model="softwareEngineering" label="softwareEngineering" border size="medium"
+          >Software Engineering</el-checkbox
+        >
+        <el-checkbox v-model="aI" label="aI" border size="medium">AI</el-checkbox>
+        <el-checkbox v-model="lowLevel" label="lowLevel" border size="medium"
+          >Low level</el-checkbox
+        >
+        <el-checkbox v-model="security" label="security" border size="medium">Security</el-checkbox>
+        <el-checkbox v-model="web" label="web" border size="medium">Web</el-checkbox>
+        <el-checkbox v-model="theoretical" label="theoretical" border size="medium"
+          >Theoretical computer sciecnce</el-checkbox
+        >
+      </div>
+    </div>
+    <div style="margin-top: 20px">
+      <div class="Save_interests">
+        <el-button type="primary" @click="saveInterests">Save</el-button>
       </div>
     </div>
   </div>
@@ -16,7 +30,27 @@ import { mapState } from 'vuex';
 
 export default {
   name: 'Interests',
-  computed: mapState(['user'])
+  data() {
+    return {
+      softwareEngineering: false,
+      aI: false,
+      lowLevel: false,
+      security: false,
+      web: false,
+      theoretical: false
+    };
+  },
+  computed: mapState(['user']),
+  methods: {
+    saveInterests() {
+      console.log(`saved interests ${this.softwareEngineering}`);
+      console.log(`saved interests ${this.aI}`);
+      console.log(`saved interests ${this.lowLevel}`);
+      console.log(`saved interests ${this.security}`);
+      console.log(`saved interests ${this.web}`);
+      console.log(`saved interests ${this.theoretical}`);
+    }
+  }
 };
 </script>
 
