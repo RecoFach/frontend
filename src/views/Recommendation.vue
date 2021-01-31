8544f675d27a8d8e07a5531c69cfa5b6b443652e
Alexander Chernykh <alexanderchernykh.ru@gmail.com>
2021-01-31 17:13:02 +0100

    Started parametrised cards

--- src/views/Recommendation.vue
+++ src/views/Recommendation.vue
@@ -1,19 +1,20 @@
 <template>
   <el-row justify="space-between" :gutter="20" type="flex" class="activities">
     <RecommendedSubject
-      :subjectName="this.subjectName"
-      :url="this.url"
-      :sws="this.sws"
-      :subjectType="this.subjectType"
-      :area="this.area"
+      v-for="item in items :key='item.subjectName'"
+      :subjectName="item.subjectName"
+      :url="item.url"
+      :sws="item.sws"
+      :subjectType="item.subjectType"
+      :area="item.area"
     />
-    <RecommendedSubject
-      subjectName="Subject2 "
-      url="http://2323.com"
-      :sws="this.sws"
-      subjectType="2"
-      area="Array"
-    />
+    <!--    <RecommendedSubject-->
+    <!--      subjectName="Subject2 "-->
+    <!--      url="http://2323.com"-->
+    <!--      :sws="this.sws"-->
+    <!--      subjectType="2"-->
+    <!--      area="Array"-->
+    <!--    />-->
   </el-row>
 </template>
 
@@ -31,17 +32,41 @@ export default {
     RecommendedSubject
   },
   data() {
+    // items:[
+    //   { subjectName: '' },
+    //   { url: '' },
+    //   { sws: '' },
+    //   { subjectType: '' },
+    //   { area: '' }
+    // ]
     return {
-      subjectName: '',
-      url: '',
-      sws: '',
-      subjectType: '',
-      area: ''
+      items: [{ subjectName: '' }, { url: '' }, { sws: '' }, { subjectType: '' }, { area: '' }]
+      // subjectName: '',
+      // url: '',
+      // sws: '',
+      // subjectType: '',
+      // area: ''
     };
   },
   mounted() {
-    const listOfRecommendations = Array;
-    axios.get(`${RECOMMENDATION}`);
+    // const listOfRecommendations = Array;
+    axios
+      .get(`${RECOMMENDATION}`)
+      .then((response) => {
+        console.log(response);
+        this.items = response.data;
+      })
+      .catch((e) => {
+        //   this.$message.error({
+        //     message: 'No fields of interest were specified, please provide some',
+        //     duration: 10000,
+        //     showClose: true
+        //   });
+        console.log(e.message);
+      })
+      .finally(() => {
+        // this.$data.loading = false;
+      });
   }
 };
 </script>
