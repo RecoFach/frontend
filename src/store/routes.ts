8fa146e515048112157d80f7b790a78eeb7e9b18
sierikov <artem.sierikov@gmail.com>
2021-01-31 23:54:22 +0100

    Try to integrate recommendations

--- src/store/routes.ts
+++ src/store/routes.ts
@@ -1,5 +1,4 @@
 export const BACKEND = `${process.env.VUE_APP_API_URL}:${process.env.VUE_APP_API_PORT}`;
 export const SIGNUP = `${BACKEND}/users/signup`;
 export const USERS = `${BACKEND}/users/`;
-export const INTERESTS = `${BACKEND}/users/interests/`;
-export const RECOMMENDATION = `${BACKEND}/users/recommend`;
+export const RECOMMENDATION = `${process.env.VUE_APP_RECO_URL}:${process.env.VUE_APP_RECO_PORT}/recommend`;
--- src/views/Recommendation.vue
+++ src/views/Recommendation.vue
@@ -4,6 +4,7 @@
     <p>Here you can select your <b>favorite topics</b> to study</p>
     <el-row :gutter="20" type="flex">
       <RecommendedSubject
+        v-loading="loading"
         v-for="(subject, index) in subjects"
         :index="index"
         :key="subject.name"
@@ -18,7 +19,9 @@
 </template>
 
 <script>
+import axios from 'axios';
 import { mapState } from 'vuex';
+import { RECOMMENDATION } from '@/store/routes';
 import RecommendedSubject from '@/components/ui/RecommendedSubject.vue';
 
 export default {
@@ -29,23 +32,51 @@ export default {
   },
   data() {
     return {
+      loading: true,
       subjects: [
         {
-          name: 'Komplexpraktikum Computer- und robotergestÔøΩtzte Chirurgie',
+          name: 'Some subject 1',
           url: 'http://google.com/',
           sws: 2,
           type: 'sem',
           tags: ['AI', 'low']
         },
-        { name: 'Test 1', url: 'http://google.com/', sws: 3, type: 'lec', tags: ['swt', 'cyber'] },
-        { name: 'Test 2', url: 'http://google.com/', sws: 3, type: 'lec', tags: ['swt', 'cyber'] },
-        { name: 'Test 3', url: 'http://google.com/', sws: 3, type: 'lec', tags: ['swt', 'cyber'] }
+        {
+          name: 'Some subject 2',
+          url: 'http://google.com/',
+          sws: 3,
+          type: 'lec',
+          tags: ['swt', 'cyber']
+        },
+        {
+          name: 'Some subject 3',
+          url: 'http://google.com/',
+          sws: 3,
+          type: 'lec',
+          tags: ['swt', 'cyber']
+        },
+        {
+          name: 'Some subject 4',
+          url: 'http://google.com/',
+          sws: 3,
+          type: 'lec',
+          tags: ['swt', 'cyber']
+        }
       ]
     };
   },
   mounted() {
-    // const listOfRecommendations = Array;
-    // axios.get(`${RECOMMENDATION}`);
+    const listOfRecommendations = Array;
+    axios.post(`${RECOMMENDATION}`, {
+      params: {
+        'Software engineering': this.user.profile.interests.includes('SOFTWARE') ? 1 : 0,
+        AI: this.user.profile.interests.includes('AI') ? 1 : 0,
+        'Low-level': this.user.profile.interests.includes('LOWLEVEL') ? 1 : 0,
+        Security: this.user.profile.interests.includes('SECURITY') ? 1 : 0,
+        Web: this.user.profile.interests.includes('WEB') ? 1 : 0,
+        Theoretical: this.user.profile.interests.includes('THEORETICAL') ? 1 : 0
+      }
+    });
   }
 };
 </script>
